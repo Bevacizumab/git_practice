655bfc3e4d6e40bada6308bbc52bdfcebe6d4510
Youngjun Park <pyoungj09@gmail.com>
2025-04-27 15:45:15 +0900

    Add fn substraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn substract(a: i32, b:i32) -> i32 {
+    a - b
+}
+
+
 fn main() {
     println!("Hello, world!");
+
+    let substraction = substract(3, 4);
+    println!("Substraction: {substraction}");
 }
